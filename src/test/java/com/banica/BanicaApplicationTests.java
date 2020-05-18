6b425afba086aa173f3c8c7d272e390d0fcbed79
stefan <stef2georg@github.com>
2020-05-18 23:36:47 +0300

    Initial Spring Boot project

---
+++ src/test/java/com/banica/BanicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
