0456c0b10094da1921b320be2cf1217c09d82f61
stefan <stef2georg@github.com>
2020-05-19 23:56:47 +0300

    Remove pong.

--- market/src/main/java/com/banica/market/MarketGrpcServer.java
+++ market/src/main/java/com/banica/market/MarketGrpcServer.java
@@ -1,6 +1,6 @@
 package com.banica.market;
 
-import com.banica.messages.Pong;
+//import com.banica.messages.Pong;
 import io.grpc.Server;
 import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
 import org.slf4j.Logger;
@@ -17,8 +17,8 @@ public class MarketGrpcServer {
 
     @PostConstruct
     public void setup() throws IOException {
-        Pong pong = Pong.newBuilder().setId(26).build();
-        LOGGER.info("gRPC server started with pong - {}", pong);
+        //Pong pong = Pong.newBuilder().setId(26).build();
+        LOGGER.info("gRPC server started with pong!");
         Server server = NettyServerBuilder.forPort(8080).build();
         server.start();
     }
